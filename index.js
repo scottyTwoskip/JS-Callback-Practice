15e43c5a747ee399c5a19e3d09dde20d005ce569
scottyTwoskip <scottyeansor@gmail.com>
2023-07-06 14:00:45 -0700

    had a hard time with this

--- index.js
+++ index.js
@@ -1,15 +1,116 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage('assets/green-character/static.gif');
+let direction = null;
+let x = 100;
+let y = 250;
 
+function move(element) {
+  element.style.position = 'fixed';
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + 'px';
+    element.style.bottom = bottom + 'px';
+  }
+
+  function moveWithArrowKeys(left, bottom, callback) {
+    let x = left;
+    let y = bottom;
+    element.style.left = x + 'px';
+    element.style.bottom = y + 'px';
+
+    document.addEventListener('keydown', function (e) {
+      if (e.repeat) return;
+      if (e.key === 'ArrowLeft') {
+        direction = 'west';
+      }
+      if (e.key === 'ArrowUp') {
+        direction = 'south'; // Change direction to 'south' for up arrow key
+      }
+      if (e.key === 'ArrowRight') {
+        direction = 'east';
+      }
+      if (e.key === 'ArrowDown') {
+        direction = 'north'; // Change direction to 'north' for down arrow key
+      }
+      callback(direction);
+    });
+
+    document.addEventListener('keyup', function (e) {
+      direction = null;
+      callback(direction);
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
+}
+
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = 'assets/green-character/static.gif';
+  } else if (direction === 'west') {
+    character.src = 'assets/green-character/west.gif';
+  } else if (direction === 'north') {
+    character.src = 'assets/green-character/north.gif';
+  } else if (direction === 'east') {
+    character.src = 'assets/green-character/east.gif';
+  } else if (direction === 'south') {
+    character.src = 'assets/green-character/south.gif';
+  }
+}
+
+function moveCharacter() {
+  if (direction === 'west') {
+    x = x - 1;
+  } else if (direction === 'north') {
+    y = y - 1;
+    character.src = 'assets/green-character/north.gif';
+  } else if (direction === 'east') {
+    x = x + 1;
+  } else if (direction === 'south') {
+    y = y + 1;
+    character.src = 'assets/green-character/south.gif';
+  }
+  character.style.left = x + 'px';
+  character.style.bottom = y + 'px';
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+// Set the interval for moveCharacter
+setInterval(moveCharacter, 1);
+
+document.addEventListener('keydown', function (e) {
+  if (e.repeat) return;
+  if (e.key === 'ArrowLeft') {
+    direction = 'west';
+  }
+  if (e.key === 'ArrowUp') {
+    direction = 'south'; // Change direction to 'south' for up arrow key
+  }
+  if (e.key === 'ArrowRight') {
+    direction = 'east';
+  }
+  if (e.key === 'ArrowDown') {
+    direction = 'north'; // Change direction to 'north' for down arrow key
+  }
+  handleDirectionChange(direction);
+});
+
+document.addEventListener('keyup', function (e) {
+  direction = null;
+  handleDirectionChange(direction);
+});
+
+move(newImage('assets/tree.png')).to(200, 450);
+move(newImage('assets/pillar.png')).to(350, 250);
+move(newImage('assets/pine-tree.png')).to(450, 350);
+move(newImage('assets/crate.png')).to(150, 350);
+move(newImage('assets/well.png')).to(500, 575);
+move(newItem('assets/sword.png')).to(500, 555);
+move(newItem('assets/shield.png')).to(165, 335);
+move(newItem('assets/staff.png')).to(600, 250);
